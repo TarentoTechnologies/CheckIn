717c7169ccc381589f974c161c4e023b54f92861
Sarthak Srinivas <sarthaksrinivas@gmail.com>
2018-05-22 15:55:10 +0530

    Can create and edit new organizations (workflow)

---
+++ app/src/main/java/com/tarento/checkin/checkin/EditOrganizationActivity.kt
@@ -0,0 +1,12 @@
+package com.tarento.checkin.checkin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class EditOrganizationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_organization)
+    }
+}
--- app/src/main/java/com/tarento/checkin/checkin/OrganizationActivity.kt
+++ app/src/main/java/com/tarento/checkin/checkin/OrganizationActivity.kt
@@ -1,12 +1,24 @@
 package com.tarento.checkin.checkin
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class OrganizationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_organization)
+    }
+
+    fun createOrganization(view: View) {
+        val intent = Intent(this, CreateOrganizationActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun editOrganization(view: View) {
+        val intent = Intent(this, EditOrganizationActivity::class.java)
+        startActivity(intent)
     }
 }
