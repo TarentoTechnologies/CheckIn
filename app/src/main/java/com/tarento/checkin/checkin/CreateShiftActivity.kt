d7005fb9e8670ec20f40b826dce67e7375f6a0d2
Sarthak Srinivas <sarthaksrinivas@gmail.com>
2018-05-22 16:10:55 +0530

    Can create and edit new shifts (workflow)

---
+++ app/src/main/java/com/tarento/checkin/checkin/CreateShiftActivity.kt
@@ -0,0 +1,12 @@
+package com.tarento.checkin.checkin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateShiftActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_shift)
+    }
+}
--- app/src/main/java/com/tarento/checkin/checkin/ShiftActivity.kt
+++ app/src/main/java/com/tarento/checkin/checkin/ShiftActivity.kt
@@ -1,12 +1,24 @@
 package com.tarento.checkin.checkin
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class ShiftActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shift)
+    }
+
+    fun createShift(view: View) {
+        val intent = Intent(this, CreateShiftActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun editShift(view: View) {
+        val intent = Intent(this, EditShiftActivity::class.java)
+        startActivity(intent)
     }
 }
