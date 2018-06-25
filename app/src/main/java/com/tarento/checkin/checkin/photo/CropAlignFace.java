b052eb6cf1c845f8a95fb1e3f35c68e2d23517e9
Sarthak Srinivas <sarthaksrinivas@gmail.com>
2018-06-25 12:21:16 +0530

    Interface for face cropping

---
+++ app/src/main/java/com/tarento/checkin/checkin/photo/CropAlignFace.java
@@ -0,0 +1,13 @@
+package com.tarento.checkin.checkin.photo;
+
+import android.widget.ImageView;
+
+public interface CropAlignFace {
+
+    public Double getDistance();
+
+    public ImageView scaleRotateTranslate();
+
+    public ImageView cropFace();
+
+}
